3a034f09e5b911a99d296f8418c2ba3e0966cd44
Ivan Starčević <istarcevic16@gmail.com>
2023-12-31 18:57:32 +0100

    add faq

--- components/faqs.tsx
+++ components/faqs.tsx
@@ -1,39 +1,53 @@
-import Accordion from '@/components/utils/accordion'
+import Accordion from "@/components/utils/accordion";
 
 export default function Faqs() {
   return (
-    <section className="bg-slate-50">
-
+    <section className="">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="py-12 md:py-20 border-t border-slate-50">
-
+        <div className="py-12 md:py-20 border-t ">
           {/* Section header */}
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h2 className="h2 font-playfair-display text-slate-800">Frequently Asked Questions</h2>
+          <div id="faq" className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+            <h2 className="h2 font-playfair-display text-slate-800">Često postavljana pitanja</h2>
           </div>
 
           {/* Faqs */}
           <ul className="max-w-3xl mx-auto divide-y divide-slate-200">
-            <Accordion title="Can I upgrade or downgrade at any time?" active>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            <Accordion title="Gdje su sve dostupne vaše usluge?" active>
+              Naše usluge su dostupne na području cijele Hrvatske. Ukoliko se vaš event održava izvan Hrvatske, javite
+              nam se i dogovorit ćemo se.
             </Accordion>
-            <Accordion title="What are the advantages of the Tidy Advanced Service?">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            <Accordion title="Naplaćujete li dodatno putne troškove?">
+              Za sve evente u Zagrebu i okolici, te u našoj podružnici u Gospiću, ne naplaćujemo putne troškove. Za sve
+              ostale evente naplaćujemo putne troškove u iznosu od 0.5 €/km.
             </Accordion>
-            <Accordion title="Are there any commissions in addition to the monthly plans?">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            <Accordion title="Što je to knjiga uspomena?">
+              Knjiga uspomena je album u koji se zalijepe sve fotografije koje se naprave na eventu. Vaši gosti mogu
+              napisati poruku uz fotografiju, a na kraju eventa knjiga uspomena je vaša. Moguće je uzeti i više knjiga
+              uspomena.
             </Accordion>
-            <Accordion title="Can I request a refund?">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            <Accordion title="Mogu li uzeti knjigu uspomena uz bilo koji paket?">
+              Da. Knjiga uspomena se može uzeti uz bilo koji paket, te se dodatno naplaćuje 50 €.
             </Accordion>
-            <Accordion title="Do you offer a discount for universities and students?">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            <Accordion title="Koje vrste događaja pokrivate s photoboothom?">
+              Naš photobooth je savršen za razne vrste događaja, uključujući vjenčanja, rođendane, korporativne evente,
+              promocije i još mnogo toga.
+            </Accordion>
+            <Accordion title="Postoji li ograničenje broja fotografija koje se mogu napraviti?">
+              Ne, nema ograničenja na broj fotografija koje se mogu snimiti tijekom vašeg događaja. Vaši gosti mogu
+              koristiti photobooth koliko god puta žele.
+            </Accordion>
+            <Accordion title="Kakve su opcije personalizacije fotografija?">
+              Nudimo razne opcije za personalizaciju fotografija, uključujući dodavanje prilagođenih okvira, teksta,
+              datuma i logotipa vašeg događaja ili tvrtke na svaku fotografiju.
+            </Accordion>
+            <Accordion title="Mogu li gosti dobivati ispise fotografija na licu mjesta?">
+              Da, gosti mogu dobiti ispis fotografija na licu mjesta. Većinom su to 2 ispisa po fotografiji, gdje jedno
+              ostavljaju u knjizi uspomena, a drugo zadržavaju za sebe.
             </Accordion>
             <span className="block border-t border-gray-200" aria-hidden="true"></span>
-          </ul >
-
+          </ul>
         </div>
       </div>
     </section>
-  )
+  );
 }
