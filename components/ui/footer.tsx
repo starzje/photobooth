75913e80b5a7e899308e45bb223514b810972cf6
Ivan Starčević <istarcevic16@gmail.com>
2024-01-01 01:56:09 +0100

    fix footer links

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
 
 export default function Footer() {
   return (
@@ -7,13 +9,15 @@ export default function Footer() {
         {/* Top area: Blocks */}
         <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12">
           {/* 1st block */}
-          <div className="sm:col-span-12 lg:col-span-4 lg:max-w-xs">
+          <div className="sm:col-span-12 lg:col-span-5 lg:max-w-xs">
             <div className="mb-2 text-2xl text-white font-bold">Photobooth Zagreb</div>
-            <div className="text-lg font-semibold text-slate-300">The smarter way to start your next idea.</div>
+            <div className="text-lg font-semibold text-slate-300">
+              Sve što trebate za savršenu zabavu na jednom mjestu.
+            </div>
           </div>
 
           {/* 2nd block */}
-          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
+          <div className="sm:col-span-6 md:col-span-3 lg:col-start-6 lg:col-span-2">
             <h6 className="text-sm text-slate-300 font-semibold mb-2">Navigacija</h6>
             <ul className="text-sm font-medium space-y-2">
               <li>
@@ -53,25 +57,29 @@ export default function Footer() {
           </div>
 
           {/* 3rd block */}
-          <div className="sm:col-span-6 md:col-span-3 lg:col-span-3">
+          <div className="sm:col-span-6 md:col-span-3 lg:col-start-8 lg:col-span-3">
             <h6 className="text-sm text-slate-300 font-semibold mb-2">Kontakt</h6>
             <ul className="text-sm font-medium space-y-2">
               <li>
-                <a href="/#" className="text-slate-500 hover:text-blue-600 transition duration-150 ease-in-out">
+                <a
+                  href="mailto:contact@photobooth-zagreb.com"
+                  className="text-slate-500 hover:text-blue-600 transition duration-150 ease-in-out flex gap-2 items-center">
+                  <HiOutlineMail />
                   contact@photobooth-zagreb.com
                 </a>
               </li>
               <li>
                 <a
-                  href="/https://www.instagram.com/photo_booth_zagreb?igsh=dXY4N252anJwdHBq&utm_source=qr"
-                  className="text-slate-500 hover:text-blue-600 transition duration-150 ease-in-out">
-                  @photo_booth_zagreb
+                  href="https://www.instagram.com/photo_booth_zagreb?igsh=dXY4N252anJwdHBq&utm_source=qr"
+                  className="text-slate-500 hover:text-blue-600 transition duration-150 ease-in-out flex gap-2 items-center">
+                  <FaInstagram /> @photo_booth_zagreb
                 </a>
               </li>
               <li>
                 <a
-                  href="/https://www.facebook.com/profile.php?id=61555007070917"
-                  className="text-slate-500 hover:text-blue-600 transition duration-150 ease-in-out">
+                  href="https://www.facebook.com/profile.php?id=61555007070917"
+                  className="text-slate-500 hover:text-blue-600 transition duration-150 ease-in-out flex gap-2 items-center">
+                  <FaFacebook />
                   PhotoBooth Zagreb
                 </a>
               </li>
