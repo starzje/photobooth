59cf7a0039f4070fca157f5ba7b909795572dc6f
Ivan Starčević <istarcevic16@gmail.com>
2025-02-16 20:30:31 +0100

    update content

--- components/pricing-tables.tsx
+++ components/pricing-tables.tsx
@@ -71,7 +71,7 @@ export default function PricingTables() {
                 xmlns="http://www.w3.org/2000/svg">
                 <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
               </svg>
-              <span>USB sa svim slikama</span>
+              <span>Slanje svih slika na mail</span>
             </li>
             <li className="flex items-center">
               <svg
@@ -166,7 +166,7 @@ export default function PricingTables() {
                 xmlns="http://www.w3.org/2000/svg">
                 <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
               </svg>
-              <span>USB sa svim slikama</span>
+              <span>Slanje svih slika na mail</span>
             </li>
             <li className="flex items-center">
               <svg
@@ -261,7 +261,7 @@ export default function PricingTables() {
                 xmlns="http://www.w3.org/2000/svg">
                 <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
               </svg>
-              <span>USB sa svim slikama</span>
+              <span>Slanje svih slika na mail</span>
             </li>
             <li className="flex items-center">
               <svg
